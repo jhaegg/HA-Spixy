7457d9bacb84825b5438b4030448c0ea5cdd8569
Johan Hägg <kaustic@gmx.co.uk>
2014-04-26 15:43:18 +0200

    Set up tox, not done. Added requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='HA-Spixy',
+      version='0.0.1',
+      description='High-Availibility Spixy',
+      author='Johan Hägg',
+      author_email='kaustic@shard.se',
+      url='https://github.com/jhaegg/HA-Spixy',
+      packages=['spixy'],
+     )
