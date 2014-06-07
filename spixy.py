46810afa907eb67b88bd215887606ab06b0e06d3
Johan Hägg <kaustic@gmx.co.uk>
2014-06-07 16:15:12 +0200

    Autojoin on connect, ident regex fix

--- spixy.py
+++ spixy.py
@@ -15,15 +15,20 @@ class RawSender(Thread):
             if command != '/quit':
                 client._send_raw(command)
 
-def print_server_notice(host, target, message):
-    print(host, target, message)
+def join_on_connect(config):
+    def wrapped(host, code, target, message):
+        if code == '001':
+            for channel in config['autojoin']:
+                client._send_raw("JOIN %s" % channel)
+
+    return wrapped
 
 if __name__ == '__main__':
     with open('config/spixy.json') as f:
         config = load(f)
 
-    client = Client(**config)
-    client.register_listener('SERVER_NOTICE', print_server_notice)
+    client = Client(**config['server'])
+    client.register_listener('REPLY', join_on_connect(config))
     console = RawSender(client)
     client.connect()
     console.start()
