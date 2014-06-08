a82b76dbb591217b5e632102e572046a65a5ebde
Johan Hägg <kaustic@gmx.co.uk>
2014-06-08 16:55:09 +0200

    Configurable trigger, multi-choice decisions

--- spixy/plugins/decision.py
+++ spixy/plugins/decision.py
@@ -10,12 +10,30 @@ class DecisionPlugin(Plugin):
         super(DecisionPlugin, self).__init__(config)
 
     def _handle_decision(self, nick, target, message, **rest):
-        if message.startswith('%d'):
-            self.send_command(nick=nick, target=target)
+        if message.startswith(self._config['trigger'] + " "):
+            self.send_command(nick=nick, target=target, message=message)
 
     def _handle_command(self, command):
+        choices = self._partition(command['message'])
+
+        if len(choices) == 1:
+            answer = choice(self._config['choices'])
+        else:
+            answer = choice(choices)
+
         if command['target'].startswith("#"):
             self._client.privmsg(target=command['target'],
-                                 message="{nick}: {answer}".format(answer=choice(self._config['choices']), **command))
+                                 message="{nick}: {answer}".format(answer=answer), **command)
         else:
-            self._client.privmsg(target=command['nick'], message=choice(self._config['choices']))
+            self._client.privmsg(target=command['nick'], message=answer)
+
+    def _partition(self, message):
+        parts = [message[len(self._config['trigger']):]]
+        for separator in self._config['separators']:
+            new_parts = set()
+            for part in parts:
+                [new_parts.add(s.strip()) for s in part.split(separator)]
+
+            parts = new_parts
+
+        return list(parts)
