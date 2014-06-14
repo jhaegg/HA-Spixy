dae78584d4372168cb851a04011a54951ff98e10
Johan Hägg <kaustic@gmx.co.uk>
2014-06-14 14:15:43 +0200

    Simple persistent storage and logging

--- spixy/plugins/plugin.py
+++ spixy/plugins/plugin.py
@@ -1,22 +1,50 @@
 from collections import deque
+import logging
 from threading import Thread
 from time import sleep
+from pickle import load, dump, PicklingError, UnpicklingError
 
 _MagicQuit = 0x00ff00ff
+_levels = {
+    'critical': logging.CRITICAL,
+    'error': logging.ERROR,
+    'warn': logging.WARN,
+    'warning': logging.WARNING,
+    'info': logging.INFO,
+    'debug': logging.DEBUG
+}
 
 
 class Plugin(Thread):
     def __init__(self, config):
         self._command_channel = deque()
-        self._config = config[self.__class__.__name__]
+        self._config = config.get(self.__class__.__name__, {})
+        self._logger = logging.getLogger(self.__class__.__name__)
+        if 'loglevel' in self._config:
+            try:
+                self._logger.setLevel(self, _levels[self._config['loglevel']])
+            except KeyError:
+                self._logger.error("Unknown logging level %s" % self._config['loglevel'])
+
         super(Plugin, self).__init__()
+        filename = self.__class__.__name__ + ".pickle"
+        try:
+            with open(filename, 'rb') as f:
+                self._store = load(f)
+        except IOError:
+            self._logger.exception("Could not open %s for reading, no persistent data loaded." % filename)
+            self._store = {}
+        except UnpicklingError:
+            self._logger.exception("Error loading persistent data from %s." % filename)
+            self._store = {}
+
         self.start()
 
     def send_command(self, **command):
         self._command_channel.append(command)
 
     def run(self):
-        while (True):
+        while True:
             try:
                 command = self._command_channel.popleft()
                 if command == _MagicQuit:
@@ -31,7 +59,14 @@ class Plugin(Thread):
         raise NotImplemented
 
     def _cleanup(self):
-        pass
+        filename = self.__class__.__name__ + ".pickle"
+        try:
+            with open(filename, 'wb') as f:
+                dump(f, self._store)
+        except IOError:
+            self._logger.exception("Could not open %s for writing, persistent data not saved." % filename)
+        except PicklingError:
+            self._logger.exception("Error saving persistent data from %s." % filename)
 
     def close(self):
         self._command_channel.append(_MagicQuit)
