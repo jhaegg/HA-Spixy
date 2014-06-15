bc8b4ac37bc48dc8fd70bbd44a9b3ec38e4a6d09
Johan Hägg <kaustic@gmx.co.uk>
2014-06-15 17:21:58 +0200

    Some folks put caps in their tags

--- spixy/plugins/title.py
+++ spixy/plugins/title.py
@@ -1,6 +1,6 @@
 from .plugin import Plugin
 
-from re import compile
+from re import compile, IGNORECASE
 
 from requests_futures.sessions import FuturesSession
 from requests.exceptions import RequestException
@@ -13,7 +13,7 @@ class TitlePlugin(Plugin):
         self._pages = {}
         self._titles_fetched = False
         self._url_match = compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-        self._title_match = compile('<title>(.+?)</title>')
+        self._title_match = compile('<title>(.+?)</title>', IGNORECASE)
         self._session = FuturesSession(max_workers=5)
 
         super(TitlePlugin, self).__init__(config)
