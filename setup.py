07e79d49a6cfcdc7fdfccc92f543cadc517bb21f
Johan Hägg <johan.hagg@shard.se>
2019-03-23 20:54:05 +0100

    Modernized dependencies

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@
 
 from distutils.core import setup
 
-setup(name='HA-Spixy',
-      version='0.0.1',
-      description='High-Availibility Spixy',
-      author='Johan Hägg',
-      author_email='kaustic@shard.se',
-      url='https://github.com/jhaegg/HA-Spixy',
-      packages=['spixy'],
-     )
+setup(
+    name='HA-Spixy',
+    version='0.0.1',
+    description='High-Availibility Spixy',
+    author='Johan Hägg',
+    author_email='kaustic@shard.se',
+    url='https://github.com/jhaegg/HA-Spixy',
+    packages=['spixy'],
+    install_requires=[
+        'requests-futures'
+    ]
+)
