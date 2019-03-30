88d594ddb5b2a67dd5626fd24f9b87423339fb5b
Johan Hägg <johan.hagg@shard.se>
2019-03-30 13:57:27 +0100

    Actual plugin system

--- spixy/main.py
+++ spixy/main.py
@@ -1,16 +1,23 @@
+from importlib import import_module
 from json import load
 
 from spixy.irc.client import Client
-from spixy.plugins.friday import FridayPlugin
+
 
 def main():
     with open('config/spixy.json') as f:
         config = load(f)
 
     client = Client(**config['server'])
-    FridayPlugin(config['FridayPlugin'], client)
+    load_plugins(config['plugins'], client)
     client.run()
 
+
+def load_plugins(plugins, client):
+    for plugin in plugins:
+        module_path, initializer = plugin['module'].split(':')
+        module = import_module(module_path)
+        getattr(module, initializer)(plugin['config'], client)
 
 if __name__ == '__main__':
     main()
