c96f59a72851c4d2bbfaa4c27a005a447da33779
Johan Hägg <johan.hagg@shard.se>
2019-03-30 14:21:08 +0100

    Updated converter plugin

--- spixy/plugins/converter.py
+++ spixy/plugins/converter.py
@@ -1,25 +1,20 @@
 import re
 
-from .plugin import Plugin
 
-
-class ConverterPlugin(Plugin):
+class ConverterPlugin():
     def __init__(self, config, client):
         client.register_listener("PRIVMSG", self._handle_conversion)
         self._client = client
+        self._config = config
         self._pattern = re.compile('[0-9]+')
-        super(ConverterPlugin, self).__init__(config)
 
-    def _handle_conversion(self, message, nick, target, **rest):
+    async def _handle_conversion(self, message, nick, target, **rest):
         if message.startswith(self._config['trigger']):
-            self.send_command(nick=nick, target=target, message=message)
-
-    def _handle_command(self, command):
-        if command['target'].startswith("#"):
-            self._client.privmsg(target=command['target'],
-                                 message="{nick}: {response}".format(response=self._get_response(command['message']), **command))
-        else:
-            self._client.privmsg(target=command['nick'], message=self._get_response(command['message']))
+            if target.startswith("#"):
+                message = "{nick}: {response}".format(response=self._get_response(message), nick=nick)
+                await self._client.privmsg(target=target, message=message)
+            else:
+                await self._client.privmsg(target=nick, message=self._get_response(message))
 
     def _get_response(self, message):
         matches = self._pattern.findall(message)
